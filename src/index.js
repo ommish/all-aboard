21e6d36073156fe92c1571b48232bcd969cecf2c
Ommi Shimizu <oshimizu15@gmail.com>
2018-02-22 16:10:24 -0500

    Update port listen

--- src/index.js
+++ src/index.js
@@ -24,6 +24,8 @@ app.get('/', (req, res) => {
   res.send('landed')
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("listening");
 });
