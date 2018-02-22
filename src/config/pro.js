0e3880e9c7243a15bbbb75cc3b4c3bfc5c6c721b
Ommi Shimizu <oshimizu15@gmail.com>
2018-02-22 16:20:33 -0500

    Add pro variables

---
+++ src/config/pro.js
@@ -0,0 +1,7 @@
+module.exports = {
+	DATABASE: 'mongodb://ommish:expresstrainmetronorth@ds243728.mlab.com:43728/all-aboard-pro',
+	GOOGLE_CLIENT_ID:
+		'941662615067-8gcjt025326839u58a9sspli3ukhpl7u.apps.googleusercontent.com',
+	GOOGLE_CLIENT_SECRET: 'WifDkeE3T4ENfYSNhb9mi60m',
+	COOKIE_KEY: 'sdnfklsnjfjNALBWJKJdfgIWO485289093EWFJ56456KJNkjnasfsdf'
+};
