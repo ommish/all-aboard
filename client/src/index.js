f7aa39b56b5265325b568b14a8f6851746b272b6
Ommi Shimizu <oshimizu15@gmail.com>
2018-03-01 18:45:46 -0500

    unregister service worker

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
-// import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 const root = document.getElementById('root');
 const store = configureStore();
 ReactDOM.render(<App store={store}/>, root);
 
-// registerServiceWorker();
+unregister();
