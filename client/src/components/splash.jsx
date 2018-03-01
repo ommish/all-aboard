31033f2031d056bba26672825d09e9562501e4bf
Ommi Shimizu <oshimizu15@gmail.com>
2018-03-01 18:12:04 -0500

    Debugging

--- client/src/components/splash.jsx
+++ client/src/components/splash.jsx
@@ -9,6 +9,7 @@ class Splash extends React.Component {
   }
 
   render() {
+    debugger
     return (
       <div>Welcome!
         <a href={`${window.location.origin}/auth/google`}>Log In or Sign Up with Google</a>
