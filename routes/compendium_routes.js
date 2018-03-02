2445735cd5b7a04793b2c8b974aedf0302de58b1
Ommi Shimizu <oshimizu15@gmail.com>
2018-03-01 23:08:32 -0500

    Add route for getting classes

--- routes/compendium_routes.js
+++ routes/compendium_routes.js
@@ -2,14 +2,20 @@ const requireLogin = require('../middlewares/require_login');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const Race = mongoose.model('Race');
+const CharClass = mongoose.model('CharClass');
+const { camelCase } = require('lodash');
 
 module.exports = (app) => {
 	app.get('/api/compendium', requireLogin, async (req, res) => {
-		const racesArr = await Race.find({});
-    const races = {};
-    racesArr.forEach((race) => {
-      races[race._id] = race;
-    });
-		res.send({ races });
+		const models = [Race, CharClass];
+		const data = {};
+		for (let i = 0; i < models.length; i++) {
+			data[models[i].modelName] = {};
+			const docs = await models[i].find({});
+			for (let j = 0; j < docs.length; j++) {
+				data[models[i].modelName][docs[j]._id] = docs[j];
+			}
+		}
+		return res.send(data);
 	});
 };
