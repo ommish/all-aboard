6d8f2ae96d8ebf63f0fd49cd025675976caa1dd9
Ommi Shimizu <oshimizu15@gmail.com>
2018-03-02 23:12:07 -0500

    Remove unnecessary json parsing

--- client/src/reducers/class_reducer.js
+++ client/src/reducers/class_reducer.js
@@ -6,14 +6,6 @@ const ClassReducer = (state, action) => {
   switch (action.type) {
     case RECEIVE_COMPENDIUM:
       newState = merge({}, action.charClasses);
-      for (const charClassId in newState) {
-        newState[charClassId].savingThrows = JSON.parse(newState[charClassId].savingThrows);
-        newState[charClassId].toolProficiencies = JSON.parse(newState[charClassId].toolProficiencies);
-        newState[charClassId].weaponProficiencies = JSON.parse(newState[charClassId].weaponProficiencies);
-        newState[charClassId].armorProficiencies = JSON.parse(newState[charClassId].armorProficiencies);
-        newState[charClassId].languages = JSON.parse(newState[charClassId].languages);
-        newState[charClassId].equipment = JSON.parse(newState[charClassId].equipment);
-      }
       break;
     default:
       break;
