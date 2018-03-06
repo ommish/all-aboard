641c24fb3b07fa3be06c24c323e09033e7861d88
Ommi Shimizu <oshimizu15@gmail.com>
2018-03-05 21:40:45 -0500

    Scrap builder

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,8 @@ import { Provider } from 'react-redux';
 import UserProfile from './components/user_profile';
 import Splash from './components/splash';
 import { AuthRoute, ProtectedRoute } from './util/route_util';
-import CharacterIndex from './components/character_sheet/character_index';
+import CharacterIndex from './components/character_index';
 import CharacterSheet from './components/character_sheet/character_sheet_container';
-import CharacterBuilder from './components/character_builder/character_builder_container';
 import './App.css';
 
 const App = ({store}) => {
@@ -16,11 +15,11 @@ const App = ({store}) => {
         <div>
           <Switch>
             <ProtectedRoute path='/users/:userId' component={UserProfile}/>
+            <ProtectedRoute path='/characters/new' component={CharacterSheet}/>
             <ProtectedRoute path='/characters/:characterId' component={CharacterSheet}/>
             <AuthRoute exact path='/' component={Splash}/>
           </Switch>
           <ProtectedRoute exact path='/users/:userId/characters' component={CharacterIndex}/>
-          <ProtectedRoute exact path='/users/:userId/new-character' component={CharacterBuilder}/>
         </div>
       </BrowserRouter>
     </Provider>
