580162c3d7e459bdb7966e5535442e589cb99ed1
Ommi Shimizu <oshimizu15@gmail.com>
2018-03-31 00:30:20 -0400

    fixed untracked files

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	DATABASE: 'mongodb://ommish:expresstrain@ds125048.mlab.com:25048/all-aboard',
-	GOOGLE_CLIENT_ID:
-		'217162430505-bite9aikgo2u6vvr4sbfko3rkgku1qk4.apps.googleusercontent.com',
-	GOOGLE_CLIENT_SECRET: '81i62x9q-Ub0qeHmBg15fzmN',
-	COOKIE_KEY: 'sjfndsjknx324oisnvdkSDILVNKSKJVDN23rwefsdsdcesdvsSFE'
-};
