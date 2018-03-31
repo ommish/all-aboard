9fd397576aafab78ef4fe5cdff5252209fe7077d
Ommi Shimizu <oshimizu15@gmail.com>
2018-03-31 00:50:51 -0400

    Track dev

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+	DATABASE: 'mongodb://ommish:getoffthistraindummy@ds125048.mlab.com:25048/all-aboard',
+	GOOGLE_CLIENT_ID:
+		'217162430505-bite9aikgo2u6vvr4sbfko3rkgku1qk4.apps.googleusercontent.com',
+	GOOGLE_CLIENT_SECRET: 'jZKu6nRPzkCDP5WAiWnSJwex',
+	COOKIE_KEY: 'doesntmatterwhatthisisheyheyheyheyheyyoooooowooyeah'
+};
+
+
