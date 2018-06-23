a4f846dfeedf6fc724d4fc8fef3bfe142b365376
Ommi Shimizu <oshimizu15@gmail.com>
2018-06-23 17:03:04 -0400

    Untrack files

--- config/pro.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	DATABASE: 'mongodb://ommish:wowyougotofflucky@ds243728.mlab.com:43728/all-aboard-pro',
-	GOOGLE_CLIENT_ID:
-		'941662615067-8gcjt025326839u58a9sspli3ukhpl7u.apps.googleusercontent.com',
-	GOOGLE_CLIENT_SECRET: '0ibrag8fZybehGecBZFR7mku',
-	COOKIE_KEY: 'doesntmatterwhatthisisheyheyheyheyheyyoooooowooyeah'
-};
-
-
